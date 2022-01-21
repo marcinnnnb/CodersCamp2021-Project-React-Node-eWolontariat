43a014272ccd5474eba154b5acfe0f52e475ecc7
MartaPejkowska <95076251+MartaPejkowska@users.noreply.github.com>
2022-01-21 19:46:28 +0100

    usuniecie zbednych prob ;)

--- src/index.js
+++ src/index.js
@@ -3,20 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Tworzeniezad from './tworzeniezad';
 import Wyswietl from './wyswietlaniezad';
-import App from './App';
-import TestowyGrid from './TestowyGrid';
+
 
 ReactDOM.render(
  
    <Wyswietl/>,
-   
-
   
 
   document.getElementById('root')
 );
 
- //  
-// {<Wyswietl />}
 
-//  <Wyswietl />,
+
