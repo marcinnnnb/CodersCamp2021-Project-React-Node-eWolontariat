c9ec5b65ad35438f19f729ae51f61d81390e5aa8
MartaPejkowska <marta.pejkowska@coderscrew.pl>
2022-01-22 22:36:13 +0100

    test wyswietlania jsona. 0 stylizacji

--- src/wyswietlaniezad.js
+++ src/wyswietlaniezad.js
@@ -1,47 +1,4 @@
 import { useEffect, useState } from "react";
-import Wolontariusz from './Wolontariusz';
-
-
-
-// export default function Wyswietl() {
-
-// const [wolontariusze, setWolontariusze] = useState([]);
-
-// useEffect(() => {
-//     Wyswietlaniezad();
-//   }, []);
-
-// async function Wyswietlaniezad() {
-//     // const res = await fetch(`https://www.npoint.io/docs/d60aef680c98884dc1dewolontariusze=${wolontariusze}`);
-//     const res = await fetch('src/wolontariusze.json');
-//     const json = await res.json();
-  
-//     setWolontariusze(json.wolontariusze);
-  // }
-  
-//   {
-//     wolontariusze.map((wolontariusz) => (
-//       <Wolontariusz name={wolontariusz.name} surname={wolontariusz.surname} nick={wolontariusz.nick} short_description={wolontariusz.short_description} description={wolontariusz.description} image={wolontariusz.image} categories={wolontariusz.categories} actions= {wolontariusz.actions} key={wolontariusz.nick} />
-//     ));
-//   }
-
-// return(
-//     <div>
-//     {wolontariusze.map((wolontariusz) => (
-//     <Wolontariusz name={wolontariusz.name} 
-//     surname={wolontariusz.surname}
-//      nick={wolontariusz.nick} 
-//      short_description={wolontariusz.short_description}
-//      description={wolontariusz.description} 
-//      image={wolontariusz.image} 
-//      categories={wolontariusz.categories} 
-//      actions= {wolontariusz.actions} 
-//      key={wolontariusz.nick} 
-//     />
-//     ))}
-//     </div>)
-
-// }
 
 export default function Wyswietl(){
 
@@ -57,14 +14,23 @@ useEffect(()=>{
 
 return(
   <div>
+  
 
-   {zadania}
+  {zadania.map(zadanie =>(
+    <div key={zadanie.id}>
+    <h2> {zadanie.title}</h2>
+    <p>{zadanie.categories}</p>
+    <p>{zadanie.amount}</p>
+    <p><img width="300px" src={zadanie.image} /></p>
+    <p>{zadanie.action_description}</p>
+    <p>{zadanie.action_short_description}</p>
+    </div>
+    
+
+  ))}
   
   </div>
 )
 
 }
 
-// {zadania.map(zadanie => (
-//   <p key={zadanie.id}>{zadanie.title} </p>
-// ))}
