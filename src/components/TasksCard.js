b8e278b773b23a5cd29cc0f6ead7f2c3eca165f9
Agnieszka Kapelanczyk <agnieszka.kapelanczyk.ak@gmail.com>
2022-01-28 21:51:14 +0100

    usunieto cardMedia

--- src/components/TasksCard.js
+++ src/components/TasksCard.js
@@ -1,5 +1,4 @@
 import Card from '@material-ui/core/Card'
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
