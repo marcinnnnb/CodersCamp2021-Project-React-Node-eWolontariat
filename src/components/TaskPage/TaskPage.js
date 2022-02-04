f9ff3d20b771960e7f0cbb1216c329dc1f41cf4e
MartaPejkowska <marta.pejkowska@coderscrew.pl>
2022-02-04 10:04:54 +0100

    poprawanstylu

--- src/components/TaskPage/TaskPage.js
+++ src/components/TaskPage/TaskPage.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import { Button, Typography, Box, TextField } from "@material-ui/core";
+import { Button, Typography, Box, TextField, Card } from "@material-ui/core";
+import { borders } from '@mui/system';
 import ProgressBar from "./ProgressBar";
 import liscik from "../../assets/img/plane.svg";
 import skrzynka from "../../assets/img/mailbox.svg";
 import avatar from "../../assets/img/facet.png";
 import CustomButton from "../CustomButton";
+import pies from "../../assets/img/pies.jpg"
 
 const TaskPage = () => {
     
@@ -24,7 +26,7 @@ const TaskPage = () => {
             height = {"100%"}
             justifyContent={"center"}
             alignItems={"center"}
-            p={"4rem"}
+            p={"2rem"}
         >
         {zadania.slice(0,1).map((zadania)=>(
             <div key={zadania.id}>
@@ -41,21 +43,25 @@ const TaskPage = () => {
                 <Box>
                     <Button variant="contained" color="primary" size="large">Zobacz wszystkie zadania </Button>
                 </Box>
-                <Box sx={{   borderRadius: 3,  boxShadow: 5, width: '90%', display:"flex", flexDirection:"column", alignItems: 'center', justifyContent: 'flex-start'}}>
-                    <Typography variant="h6" align="center">Ilu wolontariuszy potrzebujemy?</Typography>
-                    <p>{zadania.amount}</p>
-                    <p>Ilu się zapisało: {zadania.sign}</p>
+                <Card  style={{ margin:'0,7rem', padding: '2.0rem',  display:'flex',  flexDirection:'column', justifyContent:'space-between', alignItems: 'center'}}>
+                <Box >
+                    <Typography variant="h5" align="center">Ilu wolontariuszy potrzebujemy?</Typography>
+                    <Typography variant="body1" align={'center'}>{zadania.amount}</Typography>
+                    <Typography variant="body1" align={'center'}>Ilu się zapisało: {zadania.sign}</Typography>
                     <ProgressBar const completed={zadania.sign/zadania.amount*100}/>
-                    <Box sx={{display:"flex", flexDirection:"column", alignItems: 'center', justifyContent: 'space-around', height:"120px"}} >
+                    <Box sx={{display:"flex", flexDirection:"column", alignItems: 'center', justifyContent: 'space-around', height:"100px"}} >
                         <CustomButton variant="outlined" color="primary" size="large" >Udostępnij</CustomButton>
                         <Button  variant="contained" color="secondary" size="large" > Pomagam </Button>
                     </Box>
-                    
+                    </Box>
+                    </Card>
+
+                    <Card raised={true} style={{ padding: '0.8rem 2.4rem', display:'flex', flexDirection:'column', justifyContent:'space-between', alignItems: 'center'}}>
+                <Box >
+                    <Typography margin="10px" variant="h5" align="center">Zgłoszeni wolontariusze</Typography>
+                    <Button  mb="10px" variant="outlined" size="medium" align="center">Kasia z Gdańska</Button>
                 </Box>
-                <Box sx={{ borderRadius: 3,  boxShadow: 5, width: '90%', display:"flex", flexDirection:"column", alignItems: 'center', justifyContent: 'space-evenly'}}>
-                    <Typography margin="10px" variant="h6" align="center">Zgłoszeni wolontariusze</Typography>
-                    <Button  mb="10px" variant="outlined" size="medium">Kasia z Gdańska</Button>
-                </Box>
+                </Card>
                 <Box sx={{ marginTop:"10px", justifySelf:"end"}}>
                     <Box>
                         <img width="200px" src={skrzynka} alt=""/>
@@ -64,15 +70,22 @@ const TaskPage = () => {
             </Box>
           
             <Box sx={{ gridArea: 'main', alignItems:"center", width:'90%', justifyContent:"center"}}>
-                <Typography variant={"subtitle1"}>Kategorie: </Typography><Button  variant="contained" color="secondary">{zadania.categories}</Button>
+                <Typography variant={"subtitle1"}>Kategorie: </Typography>
+                <Button  variant="contained" color="secondary">{zadania.categories}</Button>
                 <p align="center"><img width="400px" src={zadania.image} alt=""/></p>
-                <p>{zadania.action_description}</p>  
+                <Typography variant="body1">{zadania.action_description}</Typography>  
                 <img width="300px" src={liscik} alt=""/>
-                <Box sx={{gridArea:"main", borderRadius: 1,  boxShadow: 5,  display:"flex", flexDirection:"column", justifyContent: 'space-evenly', marginTop:"10px", marginBottom:"20px"}} >
-                    <Typography borderBottom="1px solid " variant="h5" align="center" >Komentarze</Typography>  
-                    <Typography marginLeft="20px" variant="body" align="left" ><img height="45px" src={avatar} alt=""/>{zadania.comments}</Typography>
-                </Box>
-                <TextField label="Napisz komentarz" multiline rows={3} fullWidth/>
+
+                <Card raised={true} style={{ margin:'1rem', padding: '0.5rem 0.1rem', display:'flex', flexDirection:'column', justifyContent:'space-between', alignItems: 'center'}}>
+                <Box sx={{gridArea:"main"}}  mx='0.5rem'  >
+                    <Typography variant="h5" align="center" >Komentarze</Typography>  
+                    <Typography  variant="body2"  alignSelf={'left'}><img height="45px" src={avatar} alt=""/>{zadania.comments}</Typography>
+                    </Box>
+                    </Card>
+
+                    <Card raised={true} style={{ margin:'1rem', padding: '0.8rem 0.1rem', display:'flex', flexDirection:'column', justifyContent:'space-between', alignItems: 'center'}}>
+                    <TextField label="Napisz komentarz" multiline rows={2} fullWidth/>
+                    </Card>
             </Box>
             </Box>
             </div>
