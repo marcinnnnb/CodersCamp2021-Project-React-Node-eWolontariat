b1c1fb216b2fc042b9186bfce8100aab28ce537a
Agnieszka Bury <ag.grala@gmail.com>
2022-02-08 18:36:55 +0100

    Form styles

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,14 +1,14 @@
-import {useForm} from 'react-hook-form';
-import {useState} from 'react';
-import { Button, Box, Typography, TextField } from "@material-ui/core";
+import {useForm, Controller} from 'react-hook-form';
+import { Button, Box, Typography, TextField, Container } from "@material-ui/core";
 import SendIcon from '@material-ui/icons/Send';
 import Select from 'react-select'
 import Categories from '../../assets/data/Categories';
-import TaskPagestore from '../TaskPage/TaskPagestore';
-import { Route, Routes } from 'react-router-dom';
-import { addNewTask, selectTask } from "../../store/taskSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, Route, Routes} from 'react-router-dom';
+import { addNewTask } from "../../store/taskSlice";
+import { useDispatch } from "react-redux";
 import { DisplayTaskPage } from '../TaskPage/TaskPagestore2';
+import CustomTypography from '../../theme/CustomTypography';
+import CustomButton from '../../theme/CustomButton';
 
 
 const tytulValidation={
@@ -19,65 +19,77 @@ const amountValidation={
 } 
 
 export default function TaskForm() {
-    const{register,handleSubmit} =useForm();
+    const{register,handleSubmit,control} =useForm();
+    const navigate = useNavigate();
     const dispatch = useDispatch();
-   const onSubmit = (data,e) => {
+    const onSubmit = (data,e) => {
         e.preventDefault()
-        console.log(data)
         dispatch(addNewTask(data))
+        navigate('/TaskPage2')
          };
  
-    const [categories, setCategories] = useState([]);
-    const handleChange = (event) => {
-      setCategories(event.target.value);}
-      
+    const categories = []; 
 
 return (
-    <div>
-    <form onSubmit={handleSubmit(onSubmit)}>
-    
-    <Box sx={{display:"grid", gridTemplateColumns: "3fr 2fr", gap: 2, justifyItems: 'center', alignItems: 'space-evenly', gridTemplateRows: 'auto',
-    gridTemplateAreas: `"header header"
-  "main img"
-  "main2 main2"
-  ". button"
-  "footer footer"`}}>
+    <Container>
+        <Box id={"section-task-form"}
+            padding={"3rem 4rem"}
+            my={2}
+            justifyContent={"center"}
+            alignItems={"center"}
+        >
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Box style={{display:"grid", gridTemplateColumns: "3fr 2fr", gap: "1.6rem", justifyItems: 'center', alignItems: 'space-evenly', gridTemplateRows: 'auto',
+                gridTemplateAreas: `"header header"
+                    "main img"
+                    "main2 main2"
+                    ". button"
+                    "footer footer"`}}>
+                <Box style={{ gridArea: 'header'}}> 
+                    <CustomTypography variantcolor={"typographycolor"} variant="h2" align="center" color="tertiary" style={{marginBottom: "4rem"}}>Tworzenie zadania dla wolontariusza </CustomTypography>
+                </Box>
+
+                <Box style={{ gridArea: 'main', display:"flex",  flexDirection:"column", alignItems: 'stretch', justifyContent: 'space-around', width: '100%', marginLeft:"30px"}}>
+                    <TextField fullWidth label="Tytuł zadania" {...register('title', tytulValidation)} {...tytulValidation}/>
+                    <TextField type="number" fullWidth label="Ilu wolontariuszy potrzebujesz?" {...register('amount', amountValidation)} {...amountValidation}/>
+                </Box>
+                
+                <Box style={{ gridArea: 'img', alignItems:"center", justifyContent:"center",  backgroundColor: 'primary', p: 2, border: '1px dashed grey', height:"180px"}}>
+                <input name="image" type="file" accept="image/png, image/jpeg" {...register('image')} />
+                </Box>
+
+                <Box style={{ gridArea: 'main2', display:"flex", gap:3, flexDirection:"column", justifyContent: 'space-around', width:"97%"}}>
+                    <TextField multiline rows={4} fullWidth label="Dodaj opis zadania" {...register("action_description")} />   
+                    <TextField style={{marginBottom: "2rem"}} fullWidth multiline rows={2} label="Dodaj krótki opis widoczny na miniaturze" {...register("action_short_description")} />
+                    <Typography variant="body1">Wybierz kategorie: </Typography>
+                    <Controller
+                        control={control}
+                        defaultValue={categories[0]}
+                        name='categories'
+                        render={({ field: { onChange, value, ref } }) => (
+                        <Select
+                            inputRef={ref}
+                            label='Kategorie'
+                            options={Categories}
+                            value={value}
+                            onChange={onChange}
+                            isMulti
+                            isSearchable
+                        />
+                        )}
+                    />
+                </Box>
+                <Box sx={{  gridArea: 'button', padding:"1rem 0"}}>
+                <Routes>
+                    <Route path="/TaskPage2" element={<DisplayTaskPage />} />
+                </Routes>
+                <CustomButton size="medium" type="submit" variant="contained" endIcon={<SendIcon />} color="tertiary"  > Opublikuj zadanie</CustomButton>
+            </Box>
+        </Box>
+        </form>
+    </Box>
+  </Container>
   
-    <Box sx={{ gridArea: 'header'}}> 
-    <Typography variant="h2" align="center" color="primary" >Tworzenie zadania dla wolontariusza </Typography>
-    </Box>
-
-    <Box sx={{ gridArea: 'main', display:"flex",  flexDirection:"column", alignItems: 'stretch', justifyContent: 'space-around',width: '100%', marginLeft:"30px"}}>
-    <TextField fullWidth label="Tytuł zadania" {...register('title', tytulValidation)} {...tytulValidation}/>
-    <TextField type="number" fullWidth label="Ilu wolontariuszy potrzebujesz?" {...register('amount', amountValidation)} {...amountValidation}/>
-    </Box>
-    
-    <Box sx={{ gridArea: 'img', alignItems:"center", justifyContent:"center",  backgroundColor: 'primary', p: 2, border: '1px dashed grey', width:"70%", height:"120px"}}>
-    <input name="image" type="file" accept="image/png, image/jpeg"  />
-    </Box>
-
-    <Box sx={{ gridArea: 'main2', display:"flex", gap:3, flexDirection:"column", justifyContent: 'space-around', width:"97%"}}>
-    <TextField multiline rows={4} fullWidth label="Dodaj opis zadania" {...register("action_description")} />   
-    <TextField fullWidth multiline rows={2} label="Dodaj krótki opis widoczny na miniaturze" {...register("action_short_description")} />
-    <Typography variant="body1">Wybierz kategorie: </Typography>
-    <Select name="categories" label="Kategorie" options={Categories} value={categories} onChange={handleChange} isMulti isSearchable {...register("categories")} />
- 
-    </Box>
-    <Box sx={{  gridArea: 'button', padding:"1rem 0"}}>
-    <Routes>
-    <Route path="/TaskPage2" element={<DisplayTaskPage />} />
-    </Routes>
-    <Button   size="medium" type="submit" variant="contained" endIcon={<SendIcon />} color="primary"  > Opublikuj zadanie</Button>
-    </Box>
-    </Box>
-
-    </form>
-    
-    
-    </div>
-    //href={"/TaskPage2"}
-    
-    )
+)
     
 }
-
--- src/components/VolunteerForm/VolunteerForm.js
+++ src/components/VolunteerForm/VolunteerForm.js
@@ -1,40 +1,70 @@
-import NewVolunteer from "../Volunteer/NewVolunteer";
-import { Box, Typography, TextField } from "@material-ui/core";
-import VolunteersList from "../Volunteer/VolunteersList";
+import { Box, Typography, TextField, Container, styled, Divider } from "@material-ui/core";
 import {useForm} from 'react-hook-form';
 import Select from 'react-select'
 import Categories from '../../assets/data/Categories';
+import CustomTypography from "../../theme/CustomTypography";
+import CustomButton from "../../theme/CustomButton";
+import SendIcon from '@material-ui/icons/Send';
+
+const StyledSelect = styled(Select)(({ theme }) => ({
+    margin: "1.2rem 0",
+    '& .css-1rhbuit-multiValue': {
+        backgroundColor: theme.palette.secondary.main,
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.dark,
+        },
+    },
+    '& .css-xb97g8': {
+        backgroundColor: theme.palette.secondary.main,
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.dark,
+        },
+    },
+    '& .css-12jo7m5': {
+        color: theme.palette.secondary.contrastText,
+    },
+    '& .css-tj5bde-Svg': {
+        fill: theme.palette.secondary.contrastText,
+    }
+  }));
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
+    '& .MuiInputLabel-root': {
+        fontWeight: "600", 
+        letterSpacing: "1.1 px", 
+        fontSize: "1.2rem"
+    }
+}));
 
 const VolunteerForm = () => {
     const{register,handleSubmit} =useForm();
     const onSubmit = data => console.log(data);
 
     return (
-        <Box id={"volunteer-form"}
-            display={"flex:"}
-            height = {"100%"}
-            justifyContent={"center"}
-            alignItems={"center"}
-        >
-         <Typography variant="h2" align="center" color="secondary" >Publikowanie profilu wolontariusza</Typography>
-         <Typography paragraph align="left">Aby dodać do swojego profilu status wolontariusza wypełnij poniższy formularz:</Typography>
+        <Container>
+            <Box id={"volunteer-form"}
+                padding={"3rem 4rem"}
+                my={2}
+                justifyContent={"center"}
+                alignItems={"center"}
+            >
+            <CustomTypography variantcolor={"typographycolor"} variant="h1" align="left" color="tertiary" style={{margin: "0 0 1rem 0"}}>Publikowanie profilu wolontariusza</CustomTypography>
+            <Divider/>
+            <Typography variant="h2" style={{margin: "6rem 0 3rem 0", fontWeight: 600, fontSize: "1.6rem"}}>Aby dodać do swojego profilu status wolontariusza wypełnij poniższy formularz:</Typography>
 
-        
+                <form onSubmit={handleSubmit(onSubmit)} style={{margin: "2rem 0 3rem 0"}}>
+                    <Typography variant="h3" >Wybierz kategorie, z których chciałbyś otrzymywać powiadomienia o nowych zadaniach:</Typography>
+                    <StyledSelect style = {{marginBottom: "6rem"}}  label="Kategorie" placeholder="Wybierz kategorie" options={Categories} isMulti isSearchable {...register("categories")} />
+                    <StyledTextField multiline rows={4} fullWidth label="Napisz parę słów o sobie. W czym chciałbyś pomagać, czy masz jakieś doświadczenie związanie z wolontariatem" {...register("action_description")} /> 
+                    <Typography variant="h3"  style={{margin: "3rem 0 0.8rem 0"}} >Jeśli masz jakieś referencje dodaj je w formacie pdf</Typography>
+                        <input name="file" type="file" accept="pdf" label="" />
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Typography variant="body">Wybierz kategorie: </Typography>
-                <Select label="Kategorie" options={Categories} isMulti isSearchable {...register("categories")} />
-
-                <TextField multiline rows={4} fullWidth label="Napisz parę słów o sobie. W czym chciałbyś pomagać, czy masz jakieś doświadczenie związanie z wolontariatem" {...register("action_description")} /> 
-                <Typography paragraph>Jeśli masz jakieś referencje dodaj je w formacie pdf</Typography>
-                <input name="file" type="file" accept="pdf" label="" />
-
-            </form>
-            <NewVolunteer/>
-            <VolunteersList/>
-
-
-        </Box>
+                </form>
+                <Box align={"right"}>
+                    <CustomButton size="medium" type="submit" variant="contained" endIcon={<SendIcon />} color="tertiary"  > Opublikuj zadanie</CustomButton>
+                </Box>
+            </Box>
+        </Container>
     )
 }
 
