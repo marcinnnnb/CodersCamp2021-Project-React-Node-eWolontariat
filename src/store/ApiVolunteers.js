fc3222ac0a82ad0a328c20ff8f1b15021354cdd6
Agnieszka Kapelanczyk <agnieszka.kapelanczyk.ak@gmail.com>
2022-02-09 20:09:51 +0100

    dodanie wolontariuszy do API

--- src/store/ApiVolunteers.js
+++ src/store/ApiVolunteers.js
@@ -1,7 +1,7 @@
 export default class Api {
    
     static getData() {
-        const uri = 'https://api.npoint.io/8f474d7ed660f96cb791';
+        const uri = 'https://api.npoint.io/3f0f98a4cd7eca33c3e9';
         return fetch(uri, {
             method: 'GET'
         });
