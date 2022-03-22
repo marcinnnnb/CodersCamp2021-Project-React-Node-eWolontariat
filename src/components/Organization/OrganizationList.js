0922171b3daeb1dfc0f57598f10f757a026ae634
Marcin Barszcz <marcin.barszcz@op.pl>
2022-03-22 21:15:42 +0100

    laczenie endpointu Organizacja

--- src/components/Organization/OrganizationList.js
+++ src/components/Organization/OrganizationList.js
@@ -1,47 +1,55 @@
-import { Box, CircularProgress } from "@material-ui/core";
-import { useEffect   } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { selectAllOrganizations } from "../../store/organizationSlice";
-import { fetchOrganizations } from "../../store/organizationSlice";
-import OrganizationCard from "../OrganizationsPage/OrganizationCard";
+import { Box, CircularProgress } from '@material-ui/core';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectAllOrganizations } from '../../store/organizationSlice';
+import { fetchOrganizations } from '../../store/organizationSlice';
+import OrganizationCard from '../OrganizationsPage/OrganizationCard';
 
-const OrganizationList = ({startSlice,endSlice}) => {
+const OrganizationList = ({ startSlice, endSlice }) => {
   const dispatch = useDispatch();
-  //const organizationsList = useSelector(selectAllOrganizations);
-  const organizationsList = [];
-  const organizationsStatus = useSelector(state => state.organization.status);
-  const error = useSelector(state => state.organization.error);
-
-  let organizations =[];
+  const organizationsList = useSelector((state) => state.organization.organizations);
+  const organizationsStatus = useSelector((state) => state.organization.status);
+  const error = useSelector((state) => state.organization.error);
 
   useEffect(() => {
     if (organizationsStatus === 'idle') {
-      dispatch(fetchOrganizations())
+      dispatch(fetchOrganizations());
     }
   }, [organizationsStatus, dispatch]);
 
   let content;
 
   if (organizationsStatus === 'loading...') {
-       content = (
-        <Box style={{color: 'red'}} padding={2} align={"center"}>
-            <CircularProgress style={{margin: "2rem"}} align={"center"} color={"secondary"}/>
-        </Box>
-      );
+    content = (
+      <Box style={{ color: 'red' }} padding={2} align={'center'}>
+        <CircularProgress style={{ margin: '2rem' }} align={'center'} color={'secondary'} />
+      </Box>
+    );
   } else if (organizationsStatus === 'succeeded (:') {
-      organizations = organizationsList.organizations || [];
-      content = (
-        <Box display={'flex'} flexDirection={"row"} flexWrap={"wrap"} padding={'0 2rem 4rem 2rem'} justifyContent={'center'}>
-        {organizations?.slice(startSlice,endSlice).map((organization,id) =>{
-          return <OrganizationCard key={`item-${organization.id}`} organization={organization} id={organization.id}/>
+    content = (
+      <Box
+        display={'flex'}
+        flexDirection={'row'}
+        flexWrap={'wrap'}
+        padding={'0 2rem 4rem 2rem'}
+        justifyContent={'center'}
+      >
+        {organizationsList.length > 0 &&
+          organizationsList.slice(startSlice, endSlice).map((organization, id) => {
+            return (
+              <OrganizationCard key={`item-${organization.id}`} organization={organization} id={organization.id} />
+            );
           })}
-        </Box>
-      );
+      </Box>
+    );
   } else if (organizationsStatus === 'failed :(') {
-       content = <Box style={{color: 'red'}} padding={2} align={"center"}>ERROR: {error}</Box>;
-  }  
-    return content;        
-    
-}
+    content = (
+      <Box style={{ color: 'red' }} padding={2} align={'center'}>
+        ERROR: {error}
+      </Box>
+    );
+  }
+  return content;
+};
 
 export default OrganizationList;
